2372d431b42a5e6d685d7d87ac53b99842a8721d
Elnura D <elnurad@elnuras-air.lan>
2019-07-26 17:46:05 -0400

    committing updates

--- files/main.py
+++ files/main.py
@@ -8,31 +8,6 @@ import requests
 import json
 app = Flask(__name__)
 
-#Your API key is 76213925c4e0771dbabb037e3a681e74
-
-#Endpoint:
-#Please, use the endpoint api.openweathermap.org for your API calls
-#Example of API call:
-#api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=76213925c4e0771dbabb037e3a681e74
-
-#city = input from post request submit 
-eastern = timezone('US/Eastern')
-fmt = "%H:%M:%S %Z%z"
-
-city = "Berlin"
-url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=76213925c4e0771dbabb037e3a681e74".format(city)
-r = (requests.get(url)).json()
-weather = {
-'temp': r['main']['temp'],
-'description': r['weather'][0]['description'],
-'city': r['name'], 
-'country': r['sys']['country'],
-'humidity': r['main']['humidity'],
-'sunrise': (datetime.fromtimestamp(r['sys']['sunrise'])).strftime(fmt),
-'sunset': (datetime.fromtimestamp(r['sys']['sunset'])).strftime(fmt),
-}
-print(weather)
-
 
 @app.route('/', methods = ['GET', 'POST'])
 def get_weather():
@@ -45,11 +20,9 @@ def get_weather():
         'description': r_one['weather'][0]['description'],
         'city': r_one['name'], 
         'country': r_one['sys']['country'],
-        'humidity': r_one['main']['humidity'],
-        'sunrise': (datetime.fromtimestamp(r_one['sys']['sunrise'])).strftime(fmt),
-        'sunset': (datetime.fromtimestamp(r_one['sys']['sunset'])).strftime(fmt)
+        'humidity': r_one['main']['humidity']
         }
-        print(weather_one)
+        
 
         city_two = request.form['city_two']
         url_two = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=76213925c4e0771dbabb037e3a681e74".format(city_two)
@@ -59,9 +32,7 @@ def get_weather():
         'description': r_two['weather'][0]['description'],
         'city': r_two['name'],
         'country': r_two['sys']['country'],
-        'humidity': r_two['main']['humidity'],
-        'sunrise': (datetime.fromtimestamp(r_two['sys']['sunrise'])).strftime(fmt),
-        'sunset': (datetime.fromtimestamp(r_two['sys']['sunset'])).strftime(fmt)
+        'humidity': r_two['main']['humidity'] 
 
         }
       
